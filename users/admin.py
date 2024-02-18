74293ce57f5f587750c3587c0899e7aa43b9ff1c
Santiago-adev <alejandrodeveloper01@gmail.com>
2024-02-17 23:42:28 -0500

    create models

--- users/admin.py
+++ users/admin.py
@@ -12,3 +12,19 @@ class CareerAdmin(admin.ModelAdmin):
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'type')
     readonly_fields = ('first_name',)
+
+@admin.register(Modality)
+class ModalityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ('career_id','day','credits')
+
+@admin.register(Matter)
+class MatterAdmin(admin.ModelAdmin):
+    list_display = ('name','career_id','semester_id','credits')
+
+@admin.register(Lounge)
+class LoungeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- users/models.py
+++ users/models.py
@@ -17,6 +17,13 @@ class Career(models.Model):
     duration = models.IntegerField()
     modality = models.CharField(max_length=225)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Carreras"
+        verbose_name = "Carrera"
+
 class User(models.Model):
     email = models.EmailField()
     first_name = models.CharField(max_length=225)
@@ -29,3 +36,51 @@ class User(models.Model):
     class Meta:
         verbose_name_plural = "Usuarios"
         verbose_name = "Usuario"
+
+class Modality(models.Model):
+    name = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Modalidades"
+        verbose_name = "Modalidad"
+
+class Lounge(models.Model):
+    name = models.CharField(max_length=225)
+    capacity = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Salones"
+        verbose_name = "Salon"
+
+class Semester(models.Model):
+    career_id = models.CharField(max_length=50)
+    day = models.CharField(max_length=20)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return self.day
+    
+    class Meta:
+        verbose_name_plural = "Semestre"
+        verbose_name = "Semestre"
+
+class Matter(models.Model):
+    name = models.CharField(max_length=50)
+    career_id = models.ForeignKey(Career, on_delete=models.CASCADE)
+    semester_id = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    credits = models.IntegerField()
+    modality_id = models.ForeignKey(Modality, on_delete=models.CASCADE)
+    lounge_id = models.ForeignKey(Lounge, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Materias"
+        verbose_name = "Materia"
